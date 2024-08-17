d3ac2453a202a8599beae9f9ab3e9645b962cf98
Musthafa P A <musthafa@softway.com>
2024-08-18 00:17:53 +0530

    feat: read csv and sequentially send data to kafka topic after every 5 seconds

--- src/main/java/com/drilling_poc/controller/EventController.java
+++ src/main/java/com/drilling_poc/controller/EventController.java
@@ -1,28 +1,50 @@
 package com.drilling_poc.controller;
-
 import com.drilling_poc.service.KafkaMessagePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map; // Import the Map interface
 
 @RestController
 @RequestMapping("/producer-app")
 public class EventController {
+
     @Autowired
     private KafkaMessagePublisher publisher;
 
     @GetMapping("/publish/{message}")
     public ResponseEntity<?> publishMessage(@PathVariable String message){
-        try {
+        try{
             publisher.sendMessageToTopic(message);
-            return ResponseEntity.ok("message published success!");
+            return ResponseEntity.ok("Message sent!");
+        }catch (Exception exception){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/publish/json")
+    public ResponseEntity<?> publishJSON(@RequestBody Map<String, Object> jsonData){
+        try{
+            publisher.sendJsonMessage(jsonData);
+            return ResponseEntity.ok("JSON Sent");
         }
         catch (Exception ex){
+            System.out.println("Message" + ex.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/publish/well-log")
+    public ResponseEntity<?> publishWellLog(){
+        try{
+            publisher.sendWellLogInInterval();
+            return ResponseEntity.ok("Sending data to topic from csv is in progress...");
+        }
+        catch (Exception ex){
+            System.out.println("Message" + ex.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
+
--- src/main/java/com/drilling_poc/service/KafkaMessagePublisher.java
+++ src/main/java/com/drilling_poc/service/KafkaMessagePublisher.java
@@ -1,26 +1,84 @@
 package com.drilling_poc.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import java.io.FileReader;
+
 
 @Service
-public class KafkaMessagePublisher {
+public class KafkaMessagePublisher extends Thread{
+
     @Autowired
     private KafkaTemplate<String, Object> template;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     public void sendMessageToTopic(String message){
-        CompletableFuture<SendResult<String, Object>> future = template.send("drilling-seismic-log", message);
-        future.whenComplete((result, ex)->{
-            if(ex == null){
+        CompletableFuture<SendResult<String, Object>> future = template.send("drilling-well-0001", message);
+        future.whenComplete((result, exception)-> {
+            if(exception == null){
                 System.out.println("Send Message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]" );
-            } else {
-                System.out.println("Unable to send message=[" + message + "] due to: " +  ex.getMessage());
+            }
+            else {
+                System.out.println("Unable to send message=[" + message + "] due to: " +  exception.getMessage());
             }
         });
     }
-}
 
+    public void sendJsonMessage(Map<String, Object> jsonData) throws JsonProcessingException {
+        String jsonString = objectMapper.writeValueAsString(jsonData);
+        CompletableFuture<SendResult<String, Object>> future = template.send("drilling-well-0001", jsonString);
+        future.whenComplete((result, exception)-> {
+            if(exception == null){
+                System.out.println("Send Message=[" + "she" + "] with offset=[" + result.getRecordMetadata().offset() + "]" );
+            }
+            else {
+                System.out.println("Unable to send message=[" + "fge" + "] due to: " +  exception.getMessage());
+            }
+        });
+    }
+
+    public void sendWellLogInInterval() throws Exception {
+        String csvFile = "/Users/musthafa/softway/DAI/spark-data-processing/csv_well_data/0001.csv";
+        List<CSVRecord> recordList = new ArrayList<>();
+
+        try(FileReader reader = new FileReader(csvFile)) {
+            Iterable<CSVRecord> csvRecords = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+            for(CSVRecord record: csvRecords){
+                recordList.add(record);
+
+                if(recordList.size() % 10 == 0){
+                    System.out.println("Rows:");
+                    for(CSVRecord rec: recordList){
+//                        System.out.println(rec);
+                        Thread.sleep(300);
+                        sendMessageToTopic(Arrays.toString(rec.values()));
+                    }
+                    recordList.clear();
+                }
+            }
+
+            if(!recordList.isEmpty()){
+                System.out.println("Remaining rows:");
+                for(CSVRecord rec: recordList){
+                    System.out.println(rec);
+                }
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
